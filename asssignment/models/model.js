8379df4e39f88f7425d0ec9e860b9c356f8afbec
Suraj S Nair <surajsnair92@gmail.com>
2017-11-09 23:21:29 -0500

    pages fixed

--- asssignment/models/model.js
+++ asssignment/models/model.js
@@ -4,12 +4,15 @@
 var mongoose = require('mongoose');
 
 var connectionString = 'mongodb://localhost/cs5610'; // for local
-var db = mongoose.connect(connectionString,{useMongoClient: true});
+var db;
 if(process.env.MLAB_USERNAME) { // check if running remotely
   var username = process.env.MLAB_USERNAME; // get from environment
   var password = process.env.MLAB_PASSWORD;
   connectionString = 'mongodb://' + username + ':' + password;
   connectionString += '@ds135514.mlab.com:35514/heroku_330hh7vq';
+  db = mongoose.connect(connectionString,{useMongoClient: true});
+}
+else{
   db = mongoose.connect(connectionString,{useMongoClient: true});
 }
 module.exports = db;
